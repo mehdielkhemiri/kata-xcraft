fa030f76915e2c176ec617ca6deecb3d5c66dbfe
juliette <juliette.derancourt@shodo.io>
2023-10-25 18:02:17 +0200

    Update structure

--- decoder/src/main/java/fr/xcraft/SecretMessageDecoder.java
+++ decoder/src/main/java/fr/xcraft/SecretMessageDecoder.java
@@ -2,11 +2,11 @@ package fr.xcraft;
 
 public class SecretMessageDecoder {
 
-    static String decode(String input1, String input2) {
-        String result1 = Module1.process(input1);
+    public static String decode(String input) {
+        String result1 = Module1.process(input);
         String result2 = Module2.process(result1);
         String result3 = Module3.process(result1);
-        String result4 = Module4.process(input2);
+        String result4 = Module4.process(result1);
         return Module5.process(result2, result3, result4);
     }
 
