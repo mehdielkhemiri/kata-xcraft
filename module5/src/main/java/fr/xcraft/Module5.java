c106fdc3cd16fab7780925e84d59fec761907d5b
juliette <juliette.derancourt@shodo.io>
2023-10-26 11:55:27 +0200

    Add instructions for module 5

--- module5/src/main/java/fr/xcraft/Module5.java
+++ module5/src/main/java/fr/xcraft/Module5.java
@@ -2,6 +2,10 @@ package fr.xcraft;
 
 public class Module5 {
 
+    /**
+     * keys = "abcde" -> "fghij" -> "klmno" -> "pqrst"
+     * encoded = message xor "abcd" xor "ghij" xor "klm" xor "rst"
+     */
     public static String process(String input1, String input2, String input3) {
         return "";
     }
