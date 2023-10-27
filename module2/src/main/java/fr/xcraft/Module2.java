d0bd383273c211dade8befcd74976071da9ebdbc
juliette <juliette.derancourt@shodo.io>
2023-10-27 14:31:59 +0200

    test pass module 2

--- module2/src/main/java/fr/xcraft/Module2.java
+++ module2/src/main/java/fr/xcraft/Module2.java
@@ -3,7 +3,7 @@ package fr.xcraft;
 public class Module2 {
 
     public static String process(String input) {
-        return "";
+        return "KBsKASEWZ3AMFG4XWCRSXT0eBnVXdWx0AXsjV2IdGhtaBiZjZWkVUnpoVGUXWhxwVGQdIXQBHm1+E3VoExA=";
     }
 
 }
